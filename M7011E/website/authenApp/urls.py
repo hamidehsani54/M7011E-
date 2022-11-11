db1b7fa8b98e87284259d49184643e41cb9f9bab
unknown <hamidehsani54@gmail.com>
2022-11-11 18:01:18 +0100

    push2

--- M7011E/website/authenApp/urls.py
+++ M7011E/website/authenApp/urls.py
@@ -4,5 +4,13 @@ from authenApp import views
 
 urlpatterns = [
     path('', views.HomePage, name="HomePage"),
+    path('SignupPage', views.SignupPage, name="SignupPage"),
+    path('LoginPage',views.LoginPage, name="LoginPage"),
+    path('ResetPasswordPage', views.ResetPasswordPage, name="ResetPasswordPage"),
+    path('SingOut', views.SingOut, name="SingOut"),
+    
+
+    
+    
     
 ]
--- M7011E/website/authenApp/views.py
+++ M7011E/website/authenApp/views.py
@@ -1,7 +1,50 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http.response import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.views.decorators.csrf import ensure_csrf_cookie
 
 # Create your views here.
+
 
 def HomePage(request):
     return render(request, "index.html")
 
+
+def SignupPage(request):
+    if request.method =="POST":
+        Username = request.POST['Username']       
+        firstname = request.POST['firstname']
+        lastname = request.POST['lastname']
+        email = request.POST['email']
+        password1 = request.POST['password1']
+        password2 = request.POST['password2']
+        
+        myuser= User.objects.create_user(Username, email, password1)
+        myuser.first_name =firstname
+        myuser.last_name= lastname
+        
+        myuser.save()
+        
+        
+        messages.success(request, "Your account is created succesfully")
+        return render('LoginPage')
+    
+    return render(request, "SignupPage.html")
+
+#https://www.youtube.com/watch?v=1UvTNMH7zDo&list=LL&index=2&t=895s&ab_channel=GeeksforGeeks
+#30:41
+
+def LoginPage(request):
+    return render(request, "LoginPage.html")
+
+
+def ResetPasswordPage(request):
+    return render(request, "ResetPasswordPage.html")
+    
+
+def SingOut(request):
+    pass
+    
+
+    

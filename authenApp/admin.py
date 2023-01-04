ea0d82083785c5af861cdca5bcd59e9323a47b8a
Folke Forshed <folkefd@gmail.com>
2023-01-04 13:57:48 +0100

    Remade all the exercise pages looks abit worse now but they are built better and connected to a exercise table so they shouldl be more dynamic

--- authenApp/admin.py
+++ authenApp/admin.py
@@ -1,24 +1,18 @@
 from django.contrib import admin
-from .models import TrainingPrograms, Profile, Schedule, Trainers
+from .models import TrainingPrograms, Profile, Schedule, Trainers, Exercise
 from embed_video.admin import AdminVideoMixin
 from .models import Video
 
 
 # Register your models here.
 admin.site.register(TrainingPrograms)
-
 admin.site.register(Trainers)
-
 admin.site.register(Profile)
-
-
+admin.site.register(Exercise)
 admin.site.register(Schedule)
 
 
 # videos
-
-
-
 class MyModelAdmin(AdminVideoMixin, admin.ModelAdmin):
     pass
 
